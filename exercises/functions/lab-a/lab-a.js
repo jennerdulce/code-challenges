10ebbf92073e6306f219baf4c0982f2ec872debb
Jenner Deseo Dulce <jennerdulce@gmail.com>
2025-01-23 15:17:57 -0800

    Added solution for testSumArray

--- exercises/functions/lab-a/lab-a.js
+++ exercises/functions/lab-a/lab-a.js
@@ -73,10 +73,12 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    let firstSum = sum(sumArr[0], sumArr[1])[0];
+    let totalSum = sum(firstSum, sumArr[2])[0];
+    let concat = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${totalSum} is their sum.`;
+    return [totalSum, concat];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
